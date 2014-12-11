53d316dd8aed7220570b5e00f0d5ee6f3aa09667
coded9 <ashok.rex.2009@gmail.com>
2014-12-11 16:49:02 +0530

    Adding the package file and main file

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var mongoose = require('mongoose');
+var app = express();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+//Environments
+app.set('port',process.env.PORT || 3434);
+app.set('views',__dirname + '/views');
+app.set('view engine','jade');
+
+
+
+app.use(bodyParser());
+app.use(methodOverride());
+//app.use(app.router);
+app.use(express.static(path.join(__dirname,'public')));
+
+mongoose.connect('mongodb:localhost/Students');
+
+var mySchema = new mongoose.Schema({
+	_id : String,
+	name1 : String,
+	college : String
+});
+
+var user = mongoose.model('stud',mySchema);
+
+app.post('/new',function(req,res){
+	new user({
+	_id   :  req.body.email,
+	name1 : req.body.name1,
+	college : req.body.college
+}).save(function(err,doc){
+	if(err) res.json(err);
+	else    res.send('Successfully inserted!');
+});
+});
+
+var server = http.createServer(app).listen(app.get('port'),function(){
+	console.log('Express server listening on port '+app.get('port'));
+});
